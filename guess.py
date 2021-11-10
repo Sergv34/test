527107f823796b973c2e1e571da5679dcdcdfb9d
Sergey <91493678+Sergv34@users.noreply.github.com>
2021-11-10 15:52:03 +0300

    new test commit
    new  commit

---
+++ guess.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+number = 23
+guess = int(input('Введите целое число : '))
+if guess == number:
+    print('Поздравляю, вы угадали,')
+    print('(хотя и не выиграли никакого приза!)')
